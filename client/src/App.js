5dffdcce753e13974ae016aa364ca6f5eaa3aff0
Andrey Arsentev <andeh38@gmail.com>
2020-05-05 02:29:00 +0300

    asd

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,8 @@ import { AppNavbar } from './components/AppNavbar';
 import { UploadImage } from './components/UploadImage';
 import { CarouselComponent } from './components/Carousel';
 import { Showcase } from './components/Showcase';
-/* import { Header } from './components/expensetracker/Header';
-import { Balance } from './components/expensetracker/Balance';
-import { IncomeExpenses } from './components/expensetracker/IncomeExpenses';
-import { TransactionList } from './components/expensetracker/TransactionList';
-import { AddTransaction } from './components/expensetracker/AddTransaction'; */
 
 import { AuthProvider } from './context/AuthState';
-import { ExpenseProvider } from './context/ExpenseState';
 
 
 function App() {
@@ -24,16 +18,6 @@ function App() {
           <Showcase id="Showcase"></Showcase>
           <UploadImage id="Upload image"></UploadImage>
           <CarouselComponent id="Carousel"></CarouselComponent>
-          {/* <div id="Expense tracker">
-            <ExpenseProvider>
-              <Container>
-                <Balance></Balance>
-                <IncomeExpenses></IncomeExpenses>
-                <TransactionList></TransactionList>
-                <AddTransaction></AddTransaction>
-              </Container>
-            </ExpenseProvider>
-          </div> */}
         </AppNavbar>
       </div>
     </AuthProvider>
